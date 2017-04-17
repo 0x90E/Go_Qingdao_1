87bd91e94cd89a497c42fd201269bbca90c4c7f9
AlbertChung <albertchungx90@gmail.com>
2017-04-17 22:49:51 +0800

    DEV: Add HTML feature
    Signed-off-by: AlbertChung <albertchungx90@gmail.com>

--- main.py
+++ main.py
@@ -14,23 +14,23 @@ from com.Plugins.DomainWithHttpsPlugin import DomainWithHttpsPlugin
 from com.Plugins.DomainPortOtherPlugin import DomainPortOtherPlugin
 from com.Plugins.UrlPathDepthPlugin import UrlPathDepthPlugin
 from com.Plugins.DomainFakePayPlugin import DomainFakePayPlugin
+from com.Common.ChinazQuery import ChinazQuery
 
 if __name__ == "__main__":
     # set_proxy('127.0.0.1', 1080)
     print "Hello question one"
+
     extraction_exector = ExtractionExector()
     extraction_exector.register_plugin(ThinyUrlPlugin())
-    """
     extraction_exector.register_plugin(DomainIpPlugin())
     extraction_exector.register_plugin(Domain30dayPlugin())
     extraction_exector.register_plugin(DomainNumberPlugin())
-
     extraction_exector.register_plugin(DomainSubNumberPlugin())
     extraction_exector.register_plugin(UrlLongPlugin())
-    """
     # extraction_exector.register_plugin(DomainMultiSubPlugin())
     # extraction_exector.register_plugin(DomainWithHttpsPlugin())
     # extraction_exector.register_plugin(DomainPortOtherPlugin())
     # extraction_exector.register_plugin(UrlPathDepthPlugin())
     extraction_exector.register_plugin(DomainFakePayPlugin())
     extraction_exector.do_extract("com/Files/url.csv", "com/Files/output.csv",)
+
